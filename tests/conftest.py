0e9a27fc22aeae9d375bd9f8a560abc36bf78b43
Shivam Shrivastava <shivamshrivastava@klypup.com>
2026-08-29 10:11:34 +0530

    fix: sanitize AWS env vars in test session for hermetic stubbed clients

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
-import sys, pathlib
+import os
+import pathlib
+import sys
+
+os.environ.pop("AWS_PROFILE", None)
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "test")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "test")
+os.environ.setdefault("AWS_DEFAULT_REGION", "ap-south-1")
 
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1] / "bot"))
